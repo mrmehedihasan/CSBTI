d98a3d738f320ca2a506957d9d4ff0c59708b6fc
Silvia Islam <mehedihasan2002.myc@gmail.com>
2023-08-17 21:03:47 +0600

    Gallery, Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  output: "server",
+  integrations: [svelte(), tailwind()],
+  adapter: vercel(),
 });
